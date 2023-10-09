cd1f827e68d775e0dbec86f48028ab803f1a39be
Adil <adilibadov654@gmail.com>
2023-10-09 23:31:31 +0400

    Added homework on loops

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿namespace HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            /*Task 1*/
+
+            bool isPrime = true;
+            for (int i = 2; i <= Number / 2; i++)
+            {
+                if (Number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine("Number is a prime");
+            }
+            else
+            {
+                Console.WriteLine("Number is not a prime");
+            }
+
+
+            /*Task 2*/
+
+            for (; Number % 2 == 0 && Number > 0;)
+            {
+                Number /= 2;
+            }
+            if (Number == 1)
+            {
+                Console.WriteLine(Number + " is a power of 2");
+            }
+            else
+            {
+                Console.WriteLine(Number + " is not a power of 2");
+            }
+
+
+            /*Task 3*/
+
+            int FloorOfNumber = 0;
+
+            for (; Number > 0; Number /= 10)
+            {
+                FloorOfNumber++;
+            }
+            Console.WriteLine("Number of floors " + FloorOfNumber);
+
+        }
+    }
+}
